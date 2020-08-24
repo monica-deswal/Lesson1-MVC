ad4ee789f1017e35f8d62b96addee4c9de87db15
monica deswal <mdeswal@falabella.cl>
2020-08-24 22:51:47 +0530

    changees

--- Lesson1-MVC/Controller/ViewController.swift
+++ Lesson1-MVC/Controller/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     var viewModel = StoryViewModel()
     
+    //Changes
     override func viewDidLoad(){
         super.viewDidLoad()
         setUpInterface()
